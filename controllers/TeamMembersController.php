a66b9bdd97d1770194bd4e1501d6c5960f85ee38
David Persson <nperson@gmx.de>
2017-10-26 19:16:18 +0200

    Prepare public release.

--- controllers/TeamMembersController.php
+++ controllers/TeamMembersController.php
@@ -1,18 +1,9 @@
 <?php
 /**
- * CMS Team
+ * Copyright 2016 Atelier Disko. All rights reserved.
  *
- * Copyright (c) 2016 Atelier Disko - All rights reserved.
- *
- * Licensed under the AD General Software License v1.
- *
- * This software is proprietary and confidential. Redistribution
- * not permitted. Unless required by applicable law or agreed to
- * in writing, software distributed on an "AS IS" BASIS, WITHOUT-
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *
- * You should have received a copy of the AD General Software
- * License. If not, see https://atelierdisko.de/licenses.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
  */
 
 namespace cms_team\controllers;
@@ -22,14 +13,14 @@ use cms_team\models\TeamMembers;
 
 class TeamMembersController extends \base_core\controllers\BaseController {
 
-	use \base_core\controllers\AdminIndexOrderedTrait; // für Drag and Drop 
+	use \base_core\controllers\AdminIndexOrderedTrait;
 
-	use \base_core\controllers\AdminAddTrait; // Im cms die Möglichkeit in Form eines Buttons einen Beitrag dieses Moduls hinzuzufügen 
-	use \base_core\controllers\AdminEditTrait; // im cms button um einen beitrag zu editieren
-	use \base_core\controllers\AdminDeleteTrait; // im cms button um einen beitrag zu löschen
+	use \base_core\controllers\AdminAddTrait;
+	use \base_core\controllers\AdminEditTrait;
+	use \base_core\controllers\AdminDeleteTrait;
 
-	use \base_core\controllers\AdminOrderTrait; // im cms die Möglichkeit beitrage per drag and drop zu verschieben
-	use \base_core\controllers\AdminPublishTrait; // im cms die Möglichkeit auf einen Publish button zu drücken > benötigt Datenbank feld 'published'
+	use \base_core\controllers\AdminOrderTrait;
+	use \base_core\controllers\AdminPublishTrait;
 }
 
 ?>
