bb1fa3ac08c782120ac8414c7535aa19c26c5e36
Marius Wilms <mariuswilms@mailbox.org>
2018-06-17 21:07:24 +0200

    Include utf8mb4 migration

---
+++ data/upgrade/003-utf8mb4.sql
@@ -0,0 +1,4 @@
+ALTER TABLE team_members CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE team_members DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE vacant_team_positions CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE vacant_team_positions DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
